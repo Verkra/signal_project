58437313b29383be5884e0739cf96f3e1ae60b95
Verkra <jxmrafaldorsz@wp.pl>
2024-04-22 17:30:10 +0200

    Task 1

--- src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
@@ -7,28 +7,30 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class fileOutputStrategy implements OutputStrategy {
+// changed class name to UpperCamelCase from "fileOutputStrategy"
+public class FileOutputStrategy implements OutputStrategy {
+// changed the variable name to camelCase "baseDirectory" from "BaseDirectory"
+    private String baseDirectory;
+// changed the variable name to UPPER_SNAKE_CASE "FILE_MAP since its a final variable"
+    public final ConcurrentHashMap<String, String> FILE_MAP = new ConcurrentHashMap<>();
+// changed the constructor name to UpperCamelCase from "fileOutputStrategy"
+    public FileOutputStrategy(String baseDirectory) {
 
-    private String BaseDirectory;
-
-    public final ConcurrentHashMap<String, String> file_map = new ConcurrentHashMap<>();
-
-    public fileOutputStrategy(String baseDirectory) {
-
-        this.BaseDirectory = baseDirectory;
+        this.baseDirectory = baseDirectory;
     }
 
     @Override
     public void output(int patientId, long timestamp, String label, String data) {
         try {
             // Create the directory
-            Files.createDirectories(Paths.get(BaseDirectory));
+            Files.createDirectories(Paths.get(baseDirectory));
         } catch (IOException e) {
             System.err.println("Error creating base directory: " + e.getMessage());
             return;
         }
-        // Set the FilePath variable
-        String FilePath = file_map.computeIfAbsent(label, k -> Paths.get(BaseDirectory, label + ".txt").toString());
+        // Chenge the name from "FilePath" to camelCase "filePath". Set the filePath variable
+    
+        String filePath = FILE_MAP.computeIfAbsent(label, k -> Paths.get(baseDirectory, label + ".txt").toString());
 
         // Write the data to the file
         try (PrintWriter out = new PrintWriter(
